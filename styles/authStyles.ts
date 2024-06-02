9749a7a5bbdf5f932b47d1b0cf54db565a6147a8
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-02 00:25:39 +0000

    auth screen styles

---
+++ styles/authStyles.ts
@@ -0,0 +1,75 @@
+import { StyleSheet } from "react-native"
+
+export const AuthStyles = StyleSheet.create({
+  mainContainer: {
+    width: '90%',
+    height: '100%',
+  },
+  title: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  subTitle: {
+    textAlign: 'center',
+    color: 'gray'
+  },
+  horizontalSection: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    columnGap:10
+  },
+  horizontalLine: {
+    height: 1,
+    backgroundColor: '#D6D6D6',
+    flexGrow: 1
+  },
+  inputContainer: {
+    
+  },
+  input: {
+    height:45,
+    borderBottomWidth: 1,
+    borderBottomColor: '#D6D6D6',
+    
+  }
+  ,
+  signInButton: {
+    borderRadius: 10,
+    backgroundColor: '#0E0213',
+    height: 45,
+    justifyContent:'center',
+    alignItems:'center',
+    
+  },
+  signInText: {
+    color: 'white',
+    fontSize: 18
+  },
+  illustrationContainer: {
+    width: '100%',
+    flex: 1
+  },
+  illustrationImage: {
+    height: '100%',
+    width: '100%',
+  },
+  userSection: {
+    rowGap: 25
+  },
+  signInWithGoogle: {
+    height:45,
+    justifyContent:'center',
+    alignItems:'center',
+    width: 200,
+    alignSelf: 'center',
+    borderRadius: 10,
+    borderWidth:1,
+    borderColor: '#D6D6D6'
+  },
+  emailSignIn: {
+    gap:20
+  }
+})
