251616e002d7b8607ec608e05d83aa0faae841cf
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-04 19:00:00 +0000

    added onboarding screen

---
+++ app/onboarding/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View,Pressable } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+      <Pressable >
+        Send me to Auth
+      </Pressable>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
