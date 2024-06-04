fcd51a9eabc66b6bf29166505c5907b34121b418
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-04 19:01:35 +0000

    added template card component

---
+++ components/TemplateCard.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TemplateCard = () => {
+  return (
+    <View>
+      <Text>TemplateCard</Text>
+    </View>
+  )
+}
+
+export default TemplateCard
+
+const styles = StyleSheet.create({})
