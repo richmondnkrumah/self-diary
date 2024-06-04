cb646b104faf3e6fb9586eadcc7298e116049cc6
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-04 19:00:41 +0000

    added index file for exporting components

---
+++ components/index.ts
@@ -0,0 +1,18 @@
+import AllNoteSection from "./AllNoteSection";
+import NoteCard from "./NoteCard";
+import StickyNoteSection from "./StickyNoteSection";
+import TemplateCard from "./TemplateCard";
+import TemplateSection from "./TemplateSection";
+
+
+
+
+export {
+  AllNoteSection,
+  NoteCard,
+  StickyNoteSection,
+  TemplateCard,
+  TemplateSection
+}
+
+
