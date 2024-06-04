c1e1ad0a91a90587e2c51bdbda89ce1ff37e7a61
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-04 19:02:46 +0000

    added styles for home drawer screens

---
+++ styles/homeStyles.ts
@@ -0,0 +1,61 @@
+import { StyleSheet } from "react-native";
+
+export const DrawerStyles = StyleSheet.create({
+  headerPaddingLeft: {
+    paddingLeft: '5%'
+  },
+  headerPaddingRight: {
+    paddingRight: '5%'
+  }
+})
+
+export const HomeStyles = StyleSheet.create({
+  mainContainer: {
+    paddingLeft:'5%',
+    paddingRight: '5%',
+    backgroundColor:'white',
+    flex: 1
+  },
+  mainTitle: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    color: '#0E0213'
+  },
+  screenHeadingContainer:{
+    rowGap:10
+  },
+  tableHeadingContainer: {
+    flexDirection: 'row',
+    columnGap:20,
+  },
+  tableHeadingButtons: {
+    zIndex: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingBottom: 3
+  },
+  tableHeadingButtonActive: {
+    borderBottomColor:'#0E0213',
+    borderBottomWidth:2,
+
+  },tableHeadingText:{
+    color:'#A3A3A3',
+    fontSize:16,
+    paddingBottom:3
+  },
+  tableHeadingTextActive: {
+    fontWeight: 'bold',
+    color:'#0E0213'
+  },
+  filterIconConntainer: {
+    flex: 1,
+  },
+  horizontalLine:{
+    width:'100%',
+    height: 1,
+    backgroundColor:'#D6D6D6',
+    position: 'absolute',
+    bottom:0
+  }
+
+})
