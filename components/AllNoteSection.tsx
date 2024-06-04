92b7cce1d7e7a389059cac84ae5603d9ae7dc11c
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-04 19:20:54 +0000

    added create note button

--- components/AllNoteSection.tsx
+++ components/AllNoteSection.tsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {AddButton} from './'
+
 
 const AllNoteSection = () => {
   return (
     <View>
       <Text>AllNotesSection</Text>
+      <AddButton />
     </View>
   )
 }
