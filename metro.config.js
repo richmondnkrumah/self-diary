7aef163e3468bcca2e7716c5a8841eb251082471
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-04 19:02:20 +0000

    added support for svg files

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,19 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname)
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' })
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer")
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"]
+  };
+
+  return config;
+})();
