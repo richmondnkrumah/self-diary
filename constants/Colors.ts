4bf78d8d149fd5782d23b358c0909adbbbca8804
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-06 16:41:40 +0000

    added custom color constants

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,31 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+interface NOTE_COLOR {
+    main_color: string,
+    highlight_color: string
+  
+}
 
-export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+
+export const PRIMARY_COLOR = '#0E0213'
+export const HIGHLIGHT_GRAY_COLOR = '#D6D6D6'
+
+const MAIN_RED = '#FFEBEB'
+
+const HIGHLIGHT_RED = '#FF8585' 
+
+
+const MAIN_YELLOW = '#FFE1C2'
+const HIGHLIGHT_YELLOW = '#FFB870'
+
+export const NOTE_COLORS: NOTE_COLOR[] = [
+  {
+    main_color: MAIN_RED,
+    highlight_color: HIGHLIGHT_RED
   },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
-};
+  {
+    main_color: MAIN_YELLOW,
+    highlight_color: HIGHLIGHT_YELLOW
+  }
+]
