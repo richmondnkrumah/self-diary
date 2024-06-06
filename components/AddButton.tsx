4326da2ec359b6303086cac1d6289c3d07df6d48
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-06 16:40:16 +0000

    ui fix size

--- components/AddButton.tsx
+++ components/AddButton.tsx
@@ -4,12 +4,10 @@ import Plus from '@/assets/svg/add-plus.svg'
 
 const AddButton = () => {
   return (
-    <Pressable style={{backgroundColor: '#0E0213', width: 46, height: 46,borderRadius: 23}}>
-      <Plus width={25} height={25} fill={'#fff'}/>
+    <Pressable style={{backgroundColor: '#0E0213', width: 50, height: 50,borderRadius: 25,alignItems:'center',justifyContent: 'center'}}>
+      <Plus width={30} height={30} color={'#fff'}/>
     </Pressable>
   )
 }
 
 export default AddButton
-
-const styles = StyleSheet.create({})
